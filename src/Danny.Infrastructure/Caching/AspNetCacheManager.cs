37497665a12c8bd021686a284624f4e9428e5871
zhengwen <mail.zhengwen@gmail.com>
2016-10-25 13:19:33 +0800

    add cache code

---
+++ src/Danny.Infrastructure/Caching/AspNetCacheManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Caching;
+
+
+namespace Danny.Infrastructure.Caching
+{
+    public class AspNetCacheManager:ICacheManager
+    {
+        public T Get<T>(string key)
+        {
+            if (key == null)
+            {
+                throw new ArgumentNullException("key");
+            }
+
+            return (T) HttpRuntime.Cache.Get(key);
+        }
+
+        public void Set(string key, object value, DateTime absoluteExpiration)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException("value");
+            }
+
+            HttpRuntime.Cache.Insert(key, value, null, absoluteExpiration, TimeSpan.Zero);
+        }
+
+        public void Remove(string key)
+        {
+            if (key == null)
+            {
+                throw new ArgumentNullException("key");
+            }
+
+            HttpRuntime.Cache.Remove(key);
+        }
+
+        public void Clear()
+        {
+            var cacheEnum = HttpRuntime.Cache.GetEnumerator();
+            while (cacheEnum.MoveNext())
+            {
+                HttpRuntime.Cache.Remove(cacheEnum.Key.ToString());
+            }
+        }
+    }
+}
---
+++ src/Danny.Infrastructure/Caching/ICacheManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Danny.Infrastructure.Caching
+{
+    public interface ICacheManager
+    {
+        T Get<T>(string key);
+
+        void Set(string key, object value, DateTime absoluteExpiration);
+
+        void Remove(string key);
+
+        void Clear();
+    }
+}
