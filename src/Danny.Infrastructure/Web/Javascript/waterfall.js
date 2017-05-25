62dbae2d31817717910ffd9f5fe8bee223d9593f
zhengwen <mail.zhengwen@gmail.com>
2017-05-25 10:57:47 +0800

    增加下拉刷新加载数据

---
+++ src/Danny.Infrastructure/Web/Javascript/waterfall.js
@@ -0,0 +1,47 @@
+﻿var waterfall = (function() {
+    var getScrollTop= function () {
+        var scrollTop = 0, bodyScrollTop = 0, documentScrollTop = 0;
+        if (document.body) {
+            bodyScrollTop = document.body.scrollTop;
+        }
+        if (document.documentElement) {
+            documentScrollTop = document.documentElement.scrollTop;
+        }
+        scrollTop = (bodyScrollTop - documentScrollTop > 0) ? bodyScrollTop : documentScrollTop;
+        return scrollTop;
+    }
+
+    var getScrollHeight =function() {
+        var scrollHeight = 0, bodyScrollHeight = 0, documentScrollHeight = 0;
+        if (document.body) {
+            bodyScrollHeight = document.body.scrollHeight;
+        }
+        if (document.documentElement) {
+            documentScrollHeight = document.documentElement.scrollHeight;
+        }
+        scrollHeight = (bodyScrollHeight - documentScrollHeight > 0) ? bodyScrollHeight : documentScrollHeight;
+        return scrollHeight;
+    }
+
+    var getWindowHeight=function () {
+        var windowHeight = 0;
+        if (document.compatMode == "CSS1Compat") {
+            windowHeight = document.documentElement.clientHeight;
+        } else {
+            windowHeight = document.body.clientHeight;
+        }
+        return windowHeight;
+    }
+
+    var bindScrollEvent = function (func) {
+        $(window).bind("scroll", function() {
+            if (getScrollTop() + getWindowHeight() >= getScrollHeight()) {
+                if (func != undefined && typeof (func) == "function") {
+                    func();
+                }
+            }
+        });
+    }
+
+    return { "bindScrollEvent": bindScrollEvent }
+})();
